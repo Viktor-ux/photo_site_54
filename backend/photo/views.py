3b2a2336d3902aa34618eecc7f29d39b3f7b8182
Viktor Feoktistov <fvs_2011@mail.ru>
2023-09-20 17:04:18 +0700

    Добавлены urls

--- backend/photo/views.py
+++ backend/photo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Страница Фотографа кошки<h1>')
+
+
+def categories(request):
+    return HttpResponse('<h1>Страница категорий</h1>')
--- backend/photosite54/urls.py
+++ backend/photosite54/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # http://127.0.0.1:8000/admin/
+    path('photo/', include('photo.urls')),
 ]
