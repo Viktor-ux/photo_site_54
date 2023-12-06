4245b211527bfbe576767ca7d30f1aa3c82d5f25
Viktor Feoktistov <fvs_2011@mail.ru>
2023-12-06 17:56:55 +0700

    Переделана модель, удалены tests.py, поправлены шаблоны

--- backend/photo/models.py
+++ backend/photo/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Photo(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок для фото')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото')
     time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_updated = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
@@ -13,14 +12,3 @@ class Photo(models.Model):
 
     class Meta:
         ordering = ['time_created']
-
-
-
-import math
-
-def next_power_of_two(arr):
-    total_sum = sum(arr)
-    result = 1
-    while result < total_sum:
-        result = result << 1
-        return result
